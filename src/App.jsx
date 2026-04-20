2057358a5a1ae9c1937049e43ec49b1b5995f8aa
tobiasnielsen05 <146809344+tobiasnielsen05@users.noreply.github.com>
2026-04-20 23:07:24 +0200

    Contact.jsx created

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
 import Cart from './pages/Cart';
+import Contact from './pages/Contact';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,56 @@
+export default function Contact() {
+  
+    return (
+    <main className="contact-page">
+      <section className="contact-wrapper">
+
+        {/* Venstre side */}
+        <div className="contact-left">
+          <h1>Contact</h1>
+          <p>Lorem ipsum, Lorem ipsum</p>
+          <p>Lorem ipsum ipsum, Lorem ipsum</p>
+          
+          <div className="contact-box">
+            <p><strong>Email</strong></p>
+            <p>contact@company.com</p>
+          </div>
+          
+          <div className="contact-box">
+            <p><strong>Phone</strong></p>
+            <p>xx xx xx xx</p>
+          </div>
+          
+          <div className="contact-socials">
+            <p>Reach out to us on:</p>
+            <div className="icons">
+                <i className="fab fa-facebook-f"></i>
+                <i className="fab fa-instagram"></i>
+                <i className="fab fa-linkedin-in"></i>
+            </div>
+          </div>
+        </div>
+
+        {/* Højre side */}
+        <div className="contact-right">
+          <form>
+            <div className="form-group">
+              <label>Full Name:</label>
+              <input type="text" placeholder="Your name" />
+            </div>
+            <div className="form-group">
+              <label>Email:</label>
+              <input type="email" placeholder="Your email address" />
+            </div>
+            
+            <div className="form-group">
+              <label>Message:</label>
+              <textarea placeholder="Write your message..."></textarea>
+            </div>
+            <button type="submit">Send Message</button>
+          </form>
+        </div>
+
+      </section>
+    </main>
+  );
+}
