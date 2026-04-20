096dc70748e927a2b3f84a87af4f9edecfcdfdd1
Sar-Eib <sarah.eibye@gmail.com>
2026-04-20 14:37:54 +0200

    images moved to /public and files ready for build and deployment on github pages CI/CD

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,24 +9,51 @@ export default function Navbar() {
   const totalItems = items.reduce((sum, item) => sum + (item.quantity || 0), 0);
   const toggleMenu = () => setIsOpen(!isOpen);
 
+  // Lukker menuen helt (bruges ved klik på links)
+  const closeAll = () => setIsOpen(false);
+
   return (
     <nav className="navbar">
       <div className="nav-container">
-        <Link to="/" className="logo"><img style={{ height: '60px' }} src="../images/disc.png" alt="" /></Link>
+        <Link to="/" className="logo" onClick={closeAll}>
+            <img style={{ height: '60px' }} src="disc.png" alt="Discover Logo" />
+        </Link>
+
         <button className="burger-menu" onClick={toggleMenu}>
           {isOpen ? '✕' : '☰'}
         </button>
+
         <div className={`nav-links ${isOpen ? 'open' : ''}`}>
           
-          <Link to="/shop" className="nav-link" onClick={() => setIsOpen(false)}>Shop</Link>
-          <Link to="/" className="nav-link" onClick={() => setIsOpen(false)}>Om os</Link>
-          <Link to="/cart" className="cart-container" onClick={() => setIsOpen(false)}>
-            <img style={{ height: '25px' }} className="cart-icon" src="../images/cart-shopping-solid-full-black.svg" alt=""/>
+          {/* SHOP DROPDOWN */}
+          <div className="nav-item dropdown">
+            <Link to="/shop" className="nav-link" onClick={closeAll}>Shop</Link>
+            <div className="dropdown-menu">
+              <Link to="/shop/film" onClick={closeAll}>Film</Link>
+              <Link to="/shop/spil" onClick={closeAll}>Spil</Link>
+              <Link to="/shop/cd" onClick={closeAll}>CD</Link>
+              <Link to="/shop/lp" onClick={closeAll}>LP</Link>
+            </div>
+          </div>
+
+          {/* OM OS DROPDOWN */}
+          <div className="nav-item dropdown">
+            <Link to="/about" className="nav-link" onClick={closeAll}>Om os</Link>
+            <div className="dropdown-menu">
+              <Link to="/guide" onClick={closeAll}>Guide</Link>
+              <Link to="/salg" onClick={closeAll}>Salg</Link>
+              <Link to="/kontakt" onClick={closeAll}>Kontakt</Link>
+            </div>
+          </div>
+
+          <Link to="/cart" className="cart-container" onClick={closeAll}>
+            <img style={{ height: '25px' }} className="cart-icon" src="cart-shopping-solid-full-black.svg" alt=""/>
             {totalItems > 0 && <span className="cart-badge">{totalItems}</span>}
           </Link>
         </div>
+
         <Link to="/cart" className="floating-cart-fab">
-          <img style={{ height: '35px' }} className="cart-icon" src="../images/cart-shopping-solid-full-black.svg" alt=""/>
+          <img style={{ height: '35px' }} className="cart-icon" src="cart-shopping-solid-full-black.svg" alt=""/>
           {totalItems > 0 && <span className="cart-badge-fab">{totalItems}</span>}
         </Link>
       </div>
