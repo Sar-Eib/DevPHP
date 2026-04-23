2b56d68511944e0fba451a9fdda21441528c845c
Sar-Eib <sarah.eibye@gmail.com>
2026-04-23 10:36:24 +0200

    Railway Wordpress link added

--- src/services/api.js
+++ src/services/api.js
@@ -1,54 +1,5 @@
-//const WP_URL = "https://din-wp-side.dk/wp-json/wp/v2/posts";
-const LARAVEL_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api';
 
-//export const getProducts = async (source) => {
-  //const url = source === 'wordpress' ? WP_URL : LARAVEL_URL;
- // const url = source === 'wordpress' ? "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita" : "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=lemon";
- // const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=lemon"
-  
- // const response = await fetch(url);
-  //const data = await response.json();
-
- // return data.drinks.map((drink) => ({
- //     id: drink.idDrink,
- //     name: drink.strDrink + " (Test-" + source + ")", // Tilføjer kilden så du kan se den virker
- //     image: drink.strDrinkThumb
-  //  }));
-
-  // Her mapper vi dataen, så de ser ens ud for React, 
-  // uanset om de kommer fra WP eller Laravel
- // return data.map(item => ({
- //   id: item.id,
- //   title: source === 'wordpress' ? item.title.rendered : item.name,
-  //  price: source === 'wordpress' ? item.acf?.price : item.price,
-  //  image: source === 'wordpress' ? item.featured_image_url : item.image_path
-  //}));
-//};
-//export const getProducts = async (source) => {
-  // 1. Vælg URL baseret på kilden
-  // Vi bruger Margarita til WordPress-test og Lemon til Laravel-test
- // const url = source === 'wordpress' 
-  //  ? "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita" 
-  //  : "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=lemon";
-
-  //try {
-  //  const response = await fetch(url);
-   // const data = await response.json();
-
-    // 2. Map dataen (CocktailDB returnerer et objekt med en "drinks" liste)
-   // return data.drinks.map((drink) => ({
-   //   id: drink.idDrink,
-      // Vi tilføjer (WP) eller (Laravel) så du er 100% sikker på at skiftet virker
-   //   name: drink.strDrink + (source === 'wordpress' ? " (WP-Test)" : " (Laravel-Test)"),
-   //   image: drink.strDrinkThumb,
-   //   price: "75 kr." // Vi "hardcoder" en pris, da CocktailDB ikke har priser
-   // }));
-
- // } catch (error) {
- //   console.error("Fejl ved hentning af test-data:", error);
- //   return [];
- // }
-//};
+const WORDPRESS_URL = "https://wordpress-api-production.up.railway.app/wp-json/wp/v2/products?_embed";
 
 const LARAVEL_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api';
 
@@ -56,8 +7,8 @@ export const getProducts = async (source) => {
   let url = "";
   
   if (source === 'wordpress') {
-    // Test-cocktail (eller senere dit WP-link)
-    url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
+    // Railway Wordpress link
+    url = WORDPRESS_URL;
   } else {
     // Lukas' sti til Laravel
     url = `${LARAVEL_BASE_URL}/products`;
@@ -73,16 +24,25 @@ export const getProducts = async (source) => {
 
     const data = await response.json();
 
-    // Mapping logik
+    // Mapping
+
     if (source === 'wordpress') {
-      return data.drinks.map((drink) => ({
-        id: drink.idDrink,
-        name: drink.strDrink + " (WP)",
-        image: drink.strDrinkThumb,
-        price: "100 kr."
-      }));
+      // Wordpress mapping
+      return data.map((item) => {
+        
+        const imagePath = item._embedded?.['wp:featuredmedia']?.[0]?.source_url || "https://via.placeholder.com/150";
+
+        return {
+          id: item.id,
+          name: item.title.rendered,
+          image: imagePath,
+          
+          price: item.acf.lager ? "99 kr." : "Udsolgt", 
+          type: item.acf.product_type
+        };
+      });
     } else {
-      //Indsæt de korrekte data ift mapping
+      // Laravel mapping
       return data.map((item) => ({
         id: item.id,
         name: item.name + " (Laravel)", // Eller hvad feltet er kaldt i databasen
