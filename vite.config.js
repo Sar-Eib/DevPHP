ce4c716755f789e8c1c9240918115e0d5628b596
Sar-Eib <sarah.eibye@gmail.com>
2026-04-24 00:42:14 +0200

    Config update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: command === 'build' ? '/DevPHP/' : '/',
+    base: '/',
     server: {
       port: 3000,
     },
